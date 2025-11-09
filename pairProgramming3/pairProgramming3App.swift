02524e3eae63fac8c9ff2484a8c5ca7efe326131
alejandroavinaa <127714455+alejandroavinaa@users.noreply.github.com>
2025-11-09 10:55:31 -0800

    Initial Commit

---
+++ pairProgramming3/pairProgramming3App.swift
@@ -0,0 +1,17 @@
+//
+//  pairProgramming3App.swift
+//  pairProgramming3
+//
+//  Created by Alejandro Avina on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct pairProgramming3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
